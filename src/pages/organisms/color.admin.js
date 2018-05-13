86d32c2c1a1d695ac02eef60ad7f19acc808411e
jeff@rhinofleettracking.com <jeff.a.winkler@gmail.com>
2018-05-12 20:52:53 -0500

    added color org

---
+++ src/pages/organisms/color.admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorForm from '../molecules/color.Form';
+
+const colorAdmin = (props) => {
+  return (
+    <div className='colorAdmin'
+
+    </div>
+  )
+}
+
+export default colorAdmin;
